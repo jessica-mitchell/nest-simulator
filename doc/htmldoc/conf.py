b42785450bd4419a0b0bf5ad33cf101d74b6ab73
Jessica Mitchell <j.mitchell@fz-juelich.de>
2023-10-13 21:16:27 +0200

    opt permission denied using tmp

--- doc/htmldoc/conf.py
+++ doc/htmldoc/conf.py
@@ -71,7 +71,7 @@ panels_add_bootstrap_css = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["templates"]
 
-plantuml = "java -jar /opt/plantuml.jar"
+plantuml = "java -jar /tmp/plantuml.jar"
 sphinx_gallery_conf = {
     # path to your examples scripts
     "examples_dirs": "../../pynest/examples",
