8e7b6f79377586f4d345b2a4837f3342113cb983
Jessica Mitchell <j.mitchell@fz-juelich.de>
2023-11-21 09:01:51 +0100

    move doxyfile

--- doc/htmldoc/conf.py
+++ doc/htmldoc/conf.py
@@ -41,7 +41,7 @@ sys.path.append(pynest_dir)
 read_the_docs_build = os.environ.get("READTHEDOCS", None) == "True"
 
 if read_the_docs_build:
-    subprocess.call("cat pwd; cd ..; doxygen", shell=True)
+    subprocess.call("doxygen", shell=True)
 
 
 # -- General configuration ------------------------------------------------
