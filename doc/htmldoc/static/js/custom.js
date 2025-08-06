7ecd54a9f3d5e78cfd97f2533a5ef64099df3a58
Jessica Mitchell <j.mitchell@fz-juelich.de>
2025-08-06 11:44:47 +0200

    add custom toast

--- doc/htmldoc/static/js/custom.js
+++ doc/htmldoc/static/js/custom.js
@@ -1,4 +1,10 @@
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl, option)
+})
+
 document.addEventListener("DOMContentLoaded", function1());
+
 
 function function1(){
 
